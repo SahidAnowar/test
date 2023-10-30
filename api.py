6b30bc159c068843d0da44eba282e9d215159d1c
SahidAnowar <49405659+SahidAnowar@users.noreply.github.com>
2023-10-30 14:14:28 +0530

    Update api.py

--- api.py
+++ api.py
@@ -3,9 +3,16 @@ from transformers import DollyProcessor, DollyImageChatModel
 
 app = FastAPI()
 
-# Load Dolly v2 3b model from HuggingFace
-model = DollyImageChatModel.from_pretrained("databricks/dolly-v2-3b")
-processor = DollyProcessor.from_pretrained("databricks/dolly-v2-3b")
+# Print the version of the transformers library
+import transformers
+print("Transformers library version:", transformers.__version__)
+
+try:
+    # Load Dolly v2 3b model from HuggingFace
+    model = DollyImageChatModel.from_pretrained("databricks/dolly-v2-3b")
+    processor = DollyProcessor.from_pretrained("databricks/dolly-v2-3b")
+except Exception as e:
+    print("Error loading the model:", str(e))
 
 @app.post("/generate")
 async def generate(prompt: str):
